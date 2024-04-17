fb89b88e55a7c406a3f78a04552febdec70eb2ad
elvis <elvis.codev@gmail.com>
2024-04-16 22:47:26 -0400

    agregar paginas a url.py v3

--- onlyflans_project/onlyflans_project/urls.py
+++ onlyflans_project/onlyflans_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web.views import indice, acerca, bienvenido
+from static_pages.views import indice, acerca, bienvenido
 
 urlpatterns = [
     path('admin/', admin.site.urls),
